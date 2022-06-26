aff0fa29cfaa75df933b1d0728cde1be5ba112ef
arif <muhdarifrawi@gmail.com>
2022-06-26 14:18:12 +0800

    created connection to MonogDB.

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const cors = require("cors")
 const app = express()
 const port = 3000
 
+require("dotenv").config()
+
+const MongoUtil = require("./MongoUtil")
+const MONGO_URI = process.env.MONGO_URI
+
 app.use(cors())
 
 
 async function main(){
+
+    try {
+        const db = await MongoUtil.connect(MONGO_URI, "sample_food");
+    }
+    catch(e){
+        console.log(e);
+    }
+    finally{
+        console.log("Database connected");
+    }
+
+
     app.get('/', (req, res) => {
         res.send('Express is running.')
     })
